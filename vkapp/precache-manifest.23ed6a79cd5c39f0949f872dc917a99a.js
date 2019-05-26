c5782e97a59b76adb9cef45e144ec5c2ad4784ce
kefirch1k <maximklezovich2016@gmail.com>
2019-05-27 01:40:48 +0300

    Initial commitbot

---
+++ vkapp/precache-manifest.23ed6a79cd5c39f0949f872dc917a99a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f80bdb2638f2a2c041f7",
+    "url": "/vkapp/static/js/runtime~main.f80bdb26.js"
+  },
+  {
+    "revision": "9d3745795aa22fc4a67b",
+    "url": "/vkapp/static/js/main.9d374579.chunk.js"
+  },
+  {
+    "revision": "3c0c3af33548976e107b",
+    "url": "/vkapp/static/js/2.3c0c3af3.chunk.js"
+  },
+  {
+    "revision": "9d3745795aa22fc4a67b",
+    "url": "/vkapp/static/css/main.8b445fdb.chunk.css"
+  },
+  {
+    "revision": "3c0c3af33548976e107b",
+    "url": "/vkapp/static/css/2.9b4e7465.chunk.css"
+  },
+  {
+    "revision": "5a5a880c7a966e27858b2e73ff1ed830",
+    "url": "/vkapp/index.html"
+  }
+];
