0be318911ac93a4049a437af05da139ea9198376
releaze <50702710+releaze@users.noreply.github.com>
2019-05-27 09:17:19 +0300

    vkapp

---
+++ vkapps/precache-manifest.caa45f643f6b13f03661c6ac9ec4b5de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "88e0734f1967e51210a6",
+    "url": "/vkapps/static/js/runtime~main.88e0734f.js"
+  },
+  {
+    "revision": "9d3745795aa22fc4a67b",
+    "url": "/vkapps/static/js/main.9d374579.chunk.js"
+  },
+  {
+    "revision": "3c0c3af33548976e107b",
+    "url": "/vkapps/static/js/2.3c0c3af3.chunk.js"
+  },
+  {
+    "revision": "9d3745795aa22fc4a67b",
+    "url": "/vkapps/static/css/main.8b445fdb.chunk.css"
+  },
+  {
+    "revision": "3c0c3af33548976e107b",
+    "url": "/vkapps/static/css/2.9b4e7465.chunk.css"
+  },
+  {
+    "revision": "18055ac7c5fa5d098635b2a6bad6dd7f",
+    "url": "/vkapps/index.html"
+  }
+];
